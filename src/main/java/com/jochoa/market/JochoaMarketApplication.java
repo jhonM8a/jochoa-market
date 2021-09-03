ea20ff599d8a14e71b09ff9f291ebd4a23da380a
jhonM8a <jhon.ochoa@unillanos.edu.co>
2021-09-03 15:44:37 -0500

    Init commit

---
+++ src/main/java/com/jochoa/market/JochoaMarketApplication.java
@@ -0,0 +1,13 @@
+package com.jochoa.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JochoaMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JochoaMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jochoa/market/JochoaMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jochoa.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JochoaMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
