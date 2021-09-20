cdf8ebd937478555682a5232372dda5c9a9f33bb
jhonM8a <jhon.ochoa@unillanos.edu.co>
2021-09-20 06:26:35 -0500

    Cambios de clases

---
+++ src/main/java/com/jochoa/market/persistence/entity/ComprasProductoPK.java
@@ -0,0 +1,22 @@
+package com.jochoa.market.persistence.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Embeddable
+public class ComprasProductoPK implements Serializable {
+    @Column(name = "id_compra")
+    private Integer idCompra;
+
+    @Column(name = "id_producto")
+    private Integer idProducto;
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+}
