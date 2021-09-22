a9b21c3439033d7fdd542f8c72c8502bc056d688
jhonM8a <jhon.ochoa@unillanos.edu.co>
2021-09-22 07:08:15 -0500

    Cambios para uso de MapStruct de las entidades de BD a las entidades de dominio y viceversa

---
+++ src/main/java/com/jochoa/market/persistence/mapper/CategoryMapper.java
@@ -0,0 +1,22 @@
+package com.jochoa.market.persistence.mapper;
+
+import com.jochoa.market.domain.Category;
+import com.jochoa.market.persistence.entity.Categoria;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+    @Mappings({
+            @Mapping(source = "idCategoria", target = "categoryId"),
+            @Mapping(source = "descripcion", target = "category"),
+            @Mapping(source = "estado", target = "active")
+    })
+    Category toCategory(Categoria categoria);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "produtos", ignore = true)
+    Categoria toCategoria(Category category);
+}
